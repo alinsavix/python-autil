aa387132958a2ec95f53419e0f58bf028bb04409
TDV Alinsa <alinsa-github@lupine.org>
2022-03-22 15:43:36 -0700

    pathfix: proper annotation handling

--- pathfix.py
+++ pathfix.py
@@ -1,12 +1,11 @@
-import functools
 import inspect
 from pathlib import Path
-from typing import Any, Callable, List
+from typing import Any, Callable, List, TypeVar, cast
 
-TFunc = Callable[..., Any]
+TFunc = TypeVar("TFunc", bound=Callable[..., Any])
 
-def fix_path(func: TFunc) -> TFunc:
-    functools.wraps(func)
+def pathfix(func: TFunc) -> TFunc:
+    # functools.wraps(func)
 
     def inner(*args: Any, **kwargs: Any) -> Any:
         # spec = inspect.getfullargspec(func)
@@ -32,18 +31,22 @@ def fix_path(func: TFunc) -> TFunc:
 
         return func(*_args, **kwargs)
 
-    return inner
+    return cast(TFunc, inner)
 
 
 if __name__ == "__main__":
-    @fix_path
-    def thingie(path: Path, path2: str, int1: int, path3, path4: Path) -> None:
+    @pathfix
+    def thingie(path: Path, path2: str, int1: int, path3: Any, path4: Path, path5: Path) -> None:
+        if path.exists():
+            print("whuff")
         print(f"path: {type(path)}")
         print(f"path2: {type(path2)}")
         print(f"path3: {type(path3)}")
         print(f"path4: {type(path4)}")
+        print(f"path5: {type(path5)}")
         print(f"int1: {type(int1)}")
 
 
     # thingie(path="/tmp/path", path4="/tmp/path4", path3="/tmp/path3", path2="/tmp/path2")
-    thingie("/tmp/path", 2, 14, path3="/tmp/path3", path4="/tmp/path4")
+    thingie("/tmp/path", 2, 14, path3="/tmp/path3", path4="/tmp/path4",
+            path5=Path("/tmp/path5"))

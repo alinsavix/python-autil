9c91d5a08e9fdf9f7b4287eeffdeb0d26e1bdc4d
TDV Alinsa <alinsa-github@lupine.org>
2022-12-06 11:53:30 -0800

    timefmt: time conversions to/from hh:mm:ss.sss form

--- src/tdvutil/__init__.py
+++ src/tdvutil/__init__.py
@@ -3,6 +3,7 @@ from _tdvutil.alintrospect import alintrospect, whatis
 from _tdvutil.now import now, nowf
 from _tdvutil.pathfix import pathfix
 from _tdvutil.ppretty import ppretty
+from _tdvutil.timefmt import hms_to_sec, sec_to_hms, sec_to_shortstr
 
 __all__ = [
     "__version__",
@@ -12,4 +13,7 @@ __all__ = [
     "ppretty",
     "now",
     "nowf",
+    "sec_to_hms",
+    "sec_to_shortstr",
+    "hms_to_sec",
 ]
